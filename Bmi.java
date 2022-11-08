548ba58720eabaaf7284a41083c80d0128869922
kosukesuzuki <k.suzuki.sys22@morijyobi.ac.jp>
2022-11-08 12:31:15 +0900

    ファイル名変更

---
+++ Bmi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double he = sc.nextDouble();
+        double we = sc.nextDouble();
+        double b = we/(he * he);
+        System.out.println(Math.round(b * 100)/100.0);
+        double hei = sc.nextDouble();
+        double wei = sc.nextDouble();
+        double c = wei/(hei * hei);
+        System.out.println(Math.round(c * 100)/100.0);
+    }
+}
